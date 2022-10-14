557859aee3afebac51fb81dd26e81282512ecd8c
alexandrekarst <alexandre.karst@iadvize.com>
2022-10-14 10:38:44 +0200

    add custom button implementation example

--- Example/IAdvizeSwiftExample/IAdvizeSwiftExample/Source/ViewController.swift
+++ Example/IAdvizeSwiftExample/IAdvizeSwiftExample/Source/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import IAdvizeConversationSDK
+
+enum ChatButtonType: Int {
+    case `default` = 0
+    case custom = 1
+}
 
 class ViewController: UIViewController {
     @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var chatButtonSegmentedControl: UISegmentedControl!
+
+    var chatButtonType: ChatButtonType = .default {
+        didSet {
+            switch chatButtonType {
+            case .`default`:
+                IAdvizeSDK.shared.chatboxController.useDefaultFloatingButton = true
+                needHelpButton.isHidden = true
+            case .custom:
+                IAdvizeSDK.shared.chatboxController.useDefaultFloatingButton = false
+                needHelpButton.isHidden = false
+            }
+        }
+    }
+    
+    var isActiveTargetingRuleAvailable: Bool = false {
+        didSet {
+            needHelpButton.isEnabled = isActiveTargetingRuleAvailable
+        }
+    }
+    
+    @IBOutlet weak var needHelpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
+        IAdvizeSDK.shared.targetingController.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +50,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func actionSwapChatButton(_ sender: Any) {
+        guard let chatButtonType = ChatButtonType(rawValue: chatButtonSegmentedControl.selectedSegmentIndex) else { return }
+        self.chatButtonType = chatButtonType
+    }
 
+    @IBAction func actionNeedHelp(_ sender: Any) {
+        IAdvizeSDK.shared.chatboxController.presentChatbox()
+    }
 }
 
+extension ViewController: TargetingControllerDelegate {
+    func activeTargetingRuleAvailabilityDidUpdate(isActiveTargetingRuleAvailable: Bool) {
+        self.isActiveTargetingRuleAvailable = isActiveTargetingRuleAvailable
+    }
+}
