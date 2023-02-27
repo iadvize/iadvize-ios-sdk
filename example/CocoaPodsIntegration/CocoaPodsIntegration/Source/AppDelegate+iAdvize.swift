41848c839895ae1dee7f56a45fd12688e378209c
Jules Tréhorel <jules.trehorel@iadvize.com>
2023-02-27 11:21:32 +0100

    (misc) code cleaning

--- example/CocoaPodsIntegration/CocoaPodsIntegration/Source/AppDelegate+iAdvize.swift
+++ example/CocoaPodsIntegration/CocoaPodsIntegration/Source/AppDelegate+iAdvize.swift
@@ -16,18 +16,10 @@ extension AppDelegate {
 
         customiseIAdvizeUI()
 
-        // Switch to false to test without GDPR.
-        let shouldActivateGDPR = true
-
         // By default, the iAdvize Conversation SDK take the device language
         IAdvizeSDK.shared.targetingController.language = SDKLanguageOption.custom(value: .en)
 
-        if shouldActivateGDPR {
-            iAdvizeActivateWithGDPR()
-
-        } else {
-            iAdvizeActivate()
-        }
+        iAdvizeActivate()
     }
 
     func customiseIAdvizeUI() {
@@ -46,36 +38,20 @@ extension AppDelegate {
         if let image = UIImage(named: "AppIcon") {
             configuration.incomingMessageAvatar = IncomingMessageAvatar.image(image: image)
         }
-
         configuration.presentationStyle = .fullScreen
 
         IAdvizeSDK.shared.chatboxController.setupChatbox(configuration: configuration)
-
         IAdvizeSDK.shared.chatboxController.setFloatingButtonPosition(leftMargin: 15.0, bottomMargin: 15.0)
     }
 
     func iAdvizeActivate() {
-        // Replace "ConnectedUserIdentifier" by your user unique identifier (it should not be
-        // a personal information of your user) so they can retrieve their conversation history
-        // accross installations and devices.
-        IAdvizeSDK.shared.activate(projectId: iAdvizeProjectID,
-                                   authenticationOption: .simple(userId: iAdvizeSimpleUserId)) { success in
-            guard success else {
-                // Activation fails. You need to retry later to be able to properly activate the iAdvize Conversation SDK.
-                print("Activation failure.")
-                return
-            }
-
-            IAdvizeSDK.shared.targetingController.activateTargetingRule(targetingRule: self.iAdvizeTargetingRule)
-        }
-    }
-
-    func iAdvizeActivateWithGDPR() {
-        // To activate GDPR, you have to provide a legal information URL.
         if let legalInfoURL = URL(string: "http://yourlegalinformationurl.com/legal") {
+            let gdprOption: GDPROption = .enabled(option: .legalInformation(url: legalInfoURL))
+            // Use this one for disabling GDPR : let gdprOption: GDPROption = .disabled
+
             IAdvizeSDK.shared.activate(projectId: iAdvizeProjectID,
-                                       authenticationOption: .simple(userId: "YOUR_USER_ID"),
-                                       gdprOption: .enabled(option: .legalInformation(url: legalInfoURL))) { success in
+                                       authenticationOption: .simple(userId: iAdvizeSimpleUserId),
+                                       gdprOption: gdprOption) { success in
                 guard success else {
                     // Activation fails. You need to retry later to be able to properly activate the iAdvize Conversation SDK.
                     print("Activation failure.")
